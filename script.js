8980d79b93fbac3c2413f7a1a81deb4632e1640b
Fahim <ashfahim18@gmail.com>
2025-09-03 22:26:49 +0600

    All added

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const call_btn = document.getElementsByClassName('call-button-class')
 for (let elements of call_btn) {
     elements.addEventListener('click', function () {
-        const coin = document.getElementById('coin-box');
-        let newCoin = parseInt(coin.innerText);
-        if (newCoin >= 20) {
-            newCoin = newCoin - 20;
-            coin.innerText = newCoin.toString();
-        }
-    })
 
-    elements.addEventListener('click', function () {
-        let his_cart = document.createElement('div');
         let cart = this.parentElement.parentElement;
         let his_sec_title = cart.querySelector(".title");
         let his_sec_desc = cart.querySelector('.number');
@@ -18,20 +9,32 @@ for (let elements of call_btn) {
         his_cart_h1.innerText = his_sec_title.innerText;
         let his_cart_h2 = document.createElement('h1');
         his_cart_h2.innerText = his_sec_desc.innerText;
-        his_cart.append(his_cart_h1);
-        his_cart.append(his_cart_h2);
-        let his_section = document.getElementById('History-section-id')
-        his_section.append(his_cart);
-        const his_cart_time = document.createElement('h1');
-        his_cart_time.innerText = new Date().toLocaleTimeString();
-        his_section.append(his_cart_time);
-        let parent_cart_div = document.createElement('div');
-        parent_cart_div.append(his_cart);
-        parent_cart_div.append(his_cart_time);
-        his_section.append(parent_cart_div);
-        parent_cart_div.classList.add('history-section');
-        his_cart_h1.classList.add('service-name');
-        his_cart_h2.classList.add('service-number');
+        const coin = document.getElementById('coin-box');
+        let newCoin = parseInt(coin.innerText);
+        if (newCoin >= 20) {
+            alert('Calling \n' + his_cart_h1.innerText + '\n' + his_cart_h2.innerText);
+            newCoin = newCoin - 20;
+            coin.innerText = newCoin.toString();
+            let his_cart = document.createElement('div');
+            his_cart.append(his_cart_h1);
+            his_cart.append(his_cart_h2);
+            let his_section = document.getElementById('History-section-id')
+            his_section.append(his_cart);
+            const his_cart_time = document.createElement('h1');
+            his_cart_time.innerText = new Date().toLocaleTimeString();
+            his_section.append(his_cart_time);
+            let parent_cart_div = document.createElement('div');
+            parent_cart_div.append(his_cart);
+            parent_cart_div.append(his_cart_time);
+            his_section.append(parent_cart_div);
+            parent_cart_div.classList.add('history-section');
+            his_cart_h1.classList.add('service-name');
+            his_cart_h2.classList.add('service-number');
+        }
+        else
+        {
+             alert("You don't have enough coin");
+        }
     })
 }
 
@@ -39,11 +42,11 @@ for (let elements of call_btn) {
 
 const heart_btn = document.getElementsByClassName('heart-btn');
 const heart_cnt = document.getElementById('heart-cnt');
-for(let element of heart_btn){
-    element.addEventListener('click',function(){
+for (let element of heart_btn) {
+    element.addEventListener('click', function () {
         let new_cnt = parseInt(heart_cnt.innerText);
         new_cnt += 1;
-        heart_cnt.innerText = new_cnt.toString(); 
+        heart_cnt.innerText = new_cnt.toString();
     })
 }
 
@@ -51,11 +54,29 @@ for(let element of heart_btn){
 
 const his_sec = document.getElementById('History-section-id');
 const clear_btn = document.getElementById('clear-btn');
-clear_btn.addEventListener('click',function(){
+clear_btn.addEventListener('click', function () {
     const removeElements = his_sec.querySelector('.history-section');
     removeElements.remove();
-    
-})
+
+ })
+
+
+
+const copy_btn = document.getElementsByClassName('copy-btn');
+const copy_cnt = document.getElementById('copy-cnt');
+for(let element of copy_btn){
+    element.addEventListener('click',function(){
+
+        let new_cnt = parseInt(copy_cnt.innerText);
+        new_cnt += 1;
+        copy_cnt.innerText = new_cnt.toString();
+         const cart = this.parentElement.parentElement;
+        const copy_num = cart.querySelector('.number').innerText;
+        navigator.clipboard.writeText(copy_num);
+        alert('Copied Number: '+ copy_num);
+    })
+}
+
 
 
 
