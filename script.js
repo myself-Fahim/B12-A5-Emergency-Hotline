370b389b0e3c2e844c767ae1520acc0d1ad7e499
Fahim <ashfahim18@gmail.com>
2025-09-03 20:01:45 +0600

    js feature added

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('hello')
-
 const call_btn = document.getElementsByClassName('call-button-class')
 for (let elements of call_btn) {
     elements.addEventListener('click', function () {
@@ -11,19 +9,21 @@ for (let elements of call_btn) {
         }
     })
 
-    elements.addEventListener('click', function(){
-
+    elements.addEventListener('click', function () {
         let his_cart = document.createElement('div');
+        let cart = this.parentElement.parentElement;
+        let his_sec_title = cart.querySelector(".title");
+        let his_sec_desc = cart.querySelector('.number');
         let his_cart_h1 = document.createElement('h1');
-        his_cart_h1.innerText=document.getElementById('his_title-id').innerText;
+        his_cart_h1.innerText = his_sec_title.innerText;
         let his_cart_h2 = document.createElement('h1');
-        his_cart_h2.innerText = document.getElementById('his-desc-id').innerText;
+        his_cart_h2.innerText = his_sec_desc.innerText;
         his_cart.append(his_cart_h1);
         his_cart.append(his_cart_h2);
-        let his_section=document.getElementById('History-section-id')
+        let his_section = document.getElementById('History-section-id')
         his_section.append(his_cart);
         const his_cart_time = document.createElement('h1');
-        his_cart_time.innerText = '11:36:58 AM';
+        his_cart_time.innerText = new Date().toLocaleTimeString();
         his_section.append(his_cart_time);
         let parent_cart_div = document.createElement('div');
         parent_cart_div.append(his_cart);
@@ -34,4 +34,31 @@ for (let elements of call_btn) {
         his_cart_h2.classList.add('service-number');
     })
 }
+
+
+
+const heart_btn = document.getElementsByClassName('heart-btn');
+const heart_cnt = document.getElementById('heart-cnt');
+for(let element of heart_btn){
+    element.addEventListener('click',function(){
+        let new_cnt = parseInt(heart_cnt.innerText);
+        new_cnt += 1;
+        heart_cnt.innerText = new_cnt.toString(); 
+    })
+}
+
+
+
+const his_sec = document.getElementById('History-section-id');
+const clear_btn = document.getElementById('clear-btn');
+clear_btn.addEventListener('click',function(){
+    const removeElements = his_sec.querySelector('.history-section');
+    removeElements.remove();
+    
+})
+
+
+
+
+
 
